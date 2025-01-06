86ac5cb7f27b5d38cc6eae95ecfc01c2b340997c
SplitWolf <mat.jake1360@gmail.com>
2025-01-05 23:07:34 -0500

    test: Add uart receiver testbench

--- src/testbenches/uart_receiver_tb.vhd
+++ src/testbenches/uart_receiver_tb.vhd
@@ -10,7 +10,7 @@ end uart_receiver_tb;
 
 architecture sim of uart_receiver_tb is
     signal clock, reset, bclockx8: std_logic;
-    signal data, rdrf, frame_error, overrun_error: std_logic;
+    signal data, rdrf, readRDR, frame_error, overrun_error: std_logic;
     signal data_in, data_out: std_logic_vector(7 downto 0);
     signal sim_end : boolean := false;
     constant period: time := 40 ns;
@@ -44,8 +44,9 @@ end process baudRateGenx8;
 DUT: entity work.uart_receiver
  port map(
     i_rxd => data,
+    i_clk => clock,
     i_baudClkx8 => bclockx8,
-    i_readRDR => '1',
+    i_readRDR => readRDR,
     i_resetn => reset,
     o_oe => overrun_error,
     o_fe => frame_error,
@@ -59,7 +60,7 @@ begin
 reset <= '0', '1' after period;
 wait for period;
 
-wait for bperiod*30*9;
+wait for bperiod*135*3;
 
 sim_end <= true;
 report "Test: OK";
@@ -69,6 +70,7 @@ end process tb;
 -- Data Generator
 dataGen: process
 begin
+    readRDR <= '0';
     -- DATA = x"55"
     data_in <= x"55";
     -- Stop bits
@@ -83,8 +85,10 @@ begin
         data <= data_in(i);
         wait for bperiod*8;
     end loop;
-    -- Stop bit
+    -- Stop bit -- TODO: FRAME ERROR (DISABLED)
     data <= '1';
+    wait for bperiod*8;
+    wait for bperiod*8;
     wait for bperiod*8;
     -- DATA = x"AA"
     data_in <= x"AA";
@@ -103,6 +107,25 @@ begin
     -- Stop bit
     data <= '1';
     wait for bperiod*8;
+    readRDR <= '1';
+    -- DATA = x"47"
+    data_in <= x"47";
+    -- Stop bits
+    for i in 0 to 4 loop
+        data <= '1';
+        wait for bperiod*8;
+    end loop;   
+    -- Start Bit
+    data <= '0';
+    wait for bperiod*8;
+    for i in 0 to 7 loop
+        data <= data_in(i);
+        wait for bperiod*8;
+    end loop;
+    -- Stop bit
+    data <= '1';
+    wait for bperiod*8;
+    
 
     wait;
 end process;
